6172d711cfc4ce8ddf9b4119b11b3349ebc54985
Coding Actions <codin@MININT-SMC97CC.redmond.corp.microsoft.com>
2020-05-31 12:43:05 -0700

    First Commit

---
+++ variables.js
@@ -0,0 +1,19 @@
+console.log(20); // sandar / numbers
+console.log(20.5);
+
+// string / joldar
+console.log("Marat");
+console.log("11");
+console.log("?..!");
+
+// boolean true/false
+
+console.log(true);
+console.log(false);
+
+// undefined / null
+console.log(undefined);
+console.log(null);
+
+// symbol
+console.log('s')
